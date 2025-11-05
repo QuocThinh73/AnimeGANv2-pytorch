c62ebc6fbd303f2bde5a20f0b6a33617f2869c4a
QuocThinh73 <ttqthinh2004@gmail.com>
2025-11-05 16:56:34 +0700

    add animeganv2 discriminator

--- models/animegan/discriminator.py
+++ models/animegan/discriminator.py
@@ -3,5 +3,43 @@ import torch.nn as nn
 
 
 class Discriminator(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, in_channels: int):
+        super().__init__()
+        
+        # Convolutional blocks
+        layers = [
+            nn.Conv2d(in_channels=in_channels, out_channels=32, kernel_size=3, stride=1),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+        ]
+        
+        layers += [
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=2),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1),
+            nn.LayerNorm(num_features=128),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+        ]
+        
+        layers += [
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=2),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1),
+            nn.LayerNorm(num_features=256),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+        ]
+        
+        layers += [
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1),
+            nn.LayerNorm(num_features=256),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+        ]
+        
+        # FCN classification layer
+        layers += [
+            nn.Conv2d(in_channels=256, out_channels=1, kernel_size=3, stride=1),
+        ]
+        
+        self.model = nn.Sequential(*layers)
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.model(x)
