bcf10178356541c8e2a50ff89545fcdd43f0bfe4
QuocThinh73 <ttqthinh2004@gmail.com>
2025-11-05 17:36:40 +0700

    add padding

--- models/animegan/discriminator.py
+++ models/animegan/discriminator.py
@@ -8,35 +8,35 @@ class Discriminator(nn.Module):
         
         # Convolutional blocks
         layers = [
-            nn.Conv2d(in_channels=in_channels, out_channels=32, kernel_size=3, stride=1),
+            nn.Conv2d(in_channels=in_channels, out_channels=32, kernel_size=3, stride=1, padding=1),
             nn.LeakyReLU(negative_slope=0.2, inplace=True),
         ]
         
         layers += [
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=2),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=2, padding=1),
             nn.LeakyReLU(negative_slope=0.2, inplace=True),
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1),
-            nn.LayerNorm(num_features=128),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
-        ]
-        
-        layers += [
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=2),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1),
-            nn.LayerNorm(num_features=256),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.LayerNorm(normalized_shape=(128, 128, 128)),
             nn.LeakyReLU(negative_slope=0.2, inplace=True),
         ]
         
         layers += [
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1),
-            nn.LayerNorm(num_features=256),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=2, padding=1),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.LayerNorm(normalized_shape=(256, 64, 64)),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+        ]
+        
+        layers += [
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.LayerNorm(normalized_shape=(256, 64, 64)),
             nn.LeakyReLU(negative_slope=0.2, inplace=True),
         ]
         
         # FCN classification layer
         layers += [
-            nn.Conv2d(in_channels=256, out_channels=1, kernel_size=3, stride=1),
+            nn.Conv2d(in_channels=256, out_channels=1, kernel_size=3, stride=1, padding=1),
         ]
         
         self.model = nn.Sequential(*layers)
