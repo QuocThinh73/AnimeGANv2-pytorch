d91e8381ea815790ed917799099c111c5fbdea1e
QuocThinh73 <ttqthinh2004@gmail.com>
2025-11-05 15:26:34 +0700

    define generator class

--- models/animegan/generator.py
+++ models/animegan/generator.py
@@ -1,7 +1,12 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class Generator(nn.Module):
     def __init__(self):
+        super().__init__()
         pass
+
+    def forward(self, x):
+        return x
