5179a5c1fb06e17ecbee8852eaaa65d06b59edec
noobcoder123456789 <152793384+noobcoder123456789@users.noreply.github.com>
2025-11-08 07:54:59 +0700

    Implement TPU support in BaseTrainer
    Added support for TPU by modifying device assignment and loader wrapping.

--- trainers/base_trainer.py
+++ trainers/base_trainer.py
@@ -1,14 +1,20 @@
 import torch
 from tqdm import tqdm
 from abc import ABC, abstractmethod
+# TPU import
+import torch_xla
+import torch_xla.core.xla_model as xm
+from torch_xla.distributed.parallel_loader import MpDeviceLoader
 
 
 class BaseTrainer(ABC):
     def __init__(self, args, loader):
         self.args = args
         self.loader = loader
-        self.device = torch.device(
-            "cuda" if torch.cuda.is_available() else "cpu")
+        if args.device == "tpu":
+            self.device = xm.xla_device() # Add TPU
+        else:
+            self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     @abstractmethod
     def build_models(self): pass
@@ -22,9 +28,17 @@ class BaseTrainer(ABC):
     @abstractmethod
     def on_epoch_end(self, epoch): pass
 
+    def _wrap_loader_if_tpu(self):
+        """Add for TPU"""
+        if isinstance(self.device, torch_xla.device.XLADevice):
+            self.loader = MpDeviceLoader(self.loader, self.device)
+
     def run(self):
         self.build_models()
         self.build_optim()
+        
+        self._wrap_loader_if_tpu() # Add for TPU
+        
         step = 0
         first_epoch = self.args.start_epoch + 1 if self.args.resume else 1
 
@@ -33,6 +47,10 @@ class BaseTrainer(ABC):
             for batch in self.loader:
                 step += 1
                 self.train_one_step(batch, step)
+
+            if isinstance(self.device, torch_xla.device.XLADevice): # Add for TPU
+                xm.mark_step()
+                            
             self.on_epoch_end(epoch)
 
     @torch.no_grad()
